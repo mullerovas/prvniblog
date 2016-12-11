5c00cb7ad4466028e899b28da1415674bf063746
Silvie Mullerova <mullerova.silvie@gmail.com>
2016-12-10 16:33:12 -0800

    Muj prvni web pokus.

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+# Create your views here.
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.order_by('-created_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
